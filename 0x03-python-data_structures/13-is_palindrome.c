bfdf8730d10ea2aa9945f8211cbdcfb0e2896b9d
ESMON13 <125477208+ESMON13@users.noreply.github.com>
2023-08-12 16:50:33 +0100

    0x03-python

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * reverse_listint - reverses a linked list
+ * reverse_listint - reverses a linked lists
  * @head: pointer to the first node in the list
  * Return: pointer to the first node in the new list
  */
