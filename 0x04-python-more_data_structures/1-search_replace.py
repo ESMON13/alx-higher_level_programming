7e9def865169ce7e9c6b34984919d406e13065ed
ESMON13 <125477208+ESMON13@users.noreply.github.com>
2023-08-16 15:26:36 +0100

    0x04-python

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+# second task
 def search_replace(my_list, search, replace):
     def find_search(element):
         return element if element != search else replace
     return list(map(find_search, my_list))
-# second task
+
